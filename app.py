4621bd2d918e293a04e5c953a5ff452b8eb1f349
Deva <devapratamap21@gmail.com>
2023-06-09 18:54:50 +0700

    first commit

---
+++ app.py
@@ -0,0 +1,40 @@
+from pymongo import MongoClient
+import jwt
+from datetime import datetime, timedelta
+import hashlib
+from flask import (
+    Flask,
+    render_template,
+    jsonify,
+    request,
+    redirect,
+    url_for
+)
+from werkzeug.utils import secure_filename
+
+app = Flask(__name__)
+
+MONGODB_CONNECTION_STRING = 'mongodb+srv://deva:HikkyS123@cluster0.lqhsg9q.mongodb.net/?retryWrites=true&w=majority'
+
+client = MongoClient(MONGODB_CONNECTION_STRING)
+db = client.dbprojectakhir
+
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+@app.route('/login')
+def login():
+    return render_template('login.html')
+
+@app.route('/register')
+def register():
+    return render_template('register.html')
+
+@app.route('/home')
+def home():
+    return render_template('home.html')
+
+
+if __name__ == "__main__":
+    app.run("0.0.0.0", port=5000, debug=True)
