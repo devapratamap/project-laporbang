ac5e71c1fb72aac36aa545ebf8bbd193cf985455
Deva <devapratamap21@gmail.com>
2023-06-09 21:31:56 +0700

    tambahan beberapa dari folder templates

--- app.py
+++ app.py
@@ -23,6 +23,10 @@ db = client.dbprojectakhir
 def home():
     return render_template('home.html')
 
+@app.route('/home-admin')
+def homeAdmin():
+    return render_template('home-admin.html')
+
 @app.route('/login')
 def login():
     return render_template('login.html')
@@ -31,8 +35,25 @@ def login():
 def register():
     return render_template('register.html')
 
+@app.route('/profile')
+def profile():
+    return render_template('profile.html')
 
+@app.route('/profile-edit')
+def profileEdit():
+    return render_template('profile-edit.html')
 
+@app.route('/berita')
+def berita():
+    return render_template('berita.html')
+
+@app.route('/berita-admin')
+def beritaAdmin():
+    return render_template('berita-admin.html')
+
+@app.route('/about')
+def about():
+    return render_template('about.html')
 
 if __name__ == "__main__":
     app.run("0.0.0.0", port=5000, debug=True)
