f4fc95dc012230c5c4bde6f05e3e0c70987217b0
Deva <devapratamap21@gmail.com>
2023-06-15 22:48:34 +0700

    perbaikan responsive navbar

--- app.py
+++ app.py
@@ -220,6 +220,5 @@ def news():
         # Tangani jika user tidak ditemukan
         return redirect('/login')
 
-
 if __name__ == '__main__':
     app.run('0.0.0.0', port=5000, debug=True)
--- static/myjs.js
+++ static/myjs.js
@@ -255,3 +255,5 @@ function toggle_thumbsup(post_id, type) {
         });
     }
 }
+
+
